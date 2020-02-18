998350471ac8f389e4a4b118300d2bc8783e25f5
何泽鹏 <275688470@qq.com>
2020-02-18 21:40:21 +0800

    搭建服务端，初始化springboot项目

---
+++ annotation-server/src/main/java/com/hezepeng/annotationserver/AnnotationServerApplication.java
@@ -0,0 +1,13 @@
+package com.hezepeng.annotationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationServerApplication.class, args);
+    }
+
+}
---
+++ annotation-server/src/test/java/com/hezepeng/annotationserver/AnnotationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hezepeng.annotationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
